c4c1c858263fa45eefcc2102a3b7a85893e1850a
RithvikP <RithvikP@users.noreply.github.com>
2015-12-12 17:48:38 -0800

    Renamed and Deleted a few Java programs

--- RithvikProgramming/src/Programs/VeryBasicPerson.java
+++ RithvikProgramming/src/Programs/VeryBasicPerson.java
@@ -2,15 +2,15 @@ package Programs;
 
 import java.util.Scanner;
 
-public class Nishant {
+public class VeryBasicPerson {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		
 		String input = keyboard.nextLine();
 		
-		String[] responses={"Nish fish","Petri dish","Shaddup","Crazy","Stop it","Webassign","wat",
-				"Kaushik is crazy","waht you mean","stop "};
+		String[] responses={"fish","Petri dish","Shaddup","Crazy","Stop it","I have work","wat",
+				"You're is crazy","waht you mean","stop "};
 		
 		System.out.println(responses[(int) Math.abs(Math.cos(input.length())*10)]);
 		
